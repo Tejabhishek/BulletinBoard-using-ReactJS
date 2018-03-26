2e64a8c51cd376f91967b073181c35413566f774
Tejabhishek <tejabhishek@gmail.com>
2018-03-25 19:13:15 -0700

    Base Working Code

---
+++ README.md
@@ -0,0 +1,1 @@
+# BulletinBoard-using-ReactJS
---
+++ src/Note.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Note extends Component {
+    state = {  }
+    render() {
+        return (
+            <div className="note"></div>
+            <p> Learn </p>
+        );
+    }
+}
+
+export default Note;
